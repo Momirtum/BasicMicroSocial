b5678cb66dbb59f49051bdb6a3cb18e0e1b8f56d
emir taştan <emircpvpci12@gmail.com>
2022-03-13 17:49:09 +0300

    local to github

---
+++ client/src/PostList.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
+
+const PostList = () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPost = async () => {
+    const res = await axios.get("http://localhost:4002/posts");
+
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ with: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentList comments={post.comments} />
+          <CommentCreate postId={post.id} />
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
+};
+export default PostList;
