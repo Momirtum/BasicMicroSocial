fb2f98f5216482c1d0cce432865a151b92955c18
Emir Taştan <emircpvpci12@gmail.com>
2022-03-13 20:44:30 +0300

    Moderation and event-bus modification
    implemented CommentUpdated event to query service.
    Added moderation service.
    event store added to event-bus.

--- query/index.js
+++ query/index.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-Parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
+
+const handleEvent = (type, data) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
 
 app.get("/posts", (req, res) => {
   res.send(posts);
@@ -15,24 +43,19 @@ app.get("/posts", (req, res) => {
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
-  if (type === "PostCreated") {
-    const { id, title } = data;
-
-    posts[id] = { id, title, comments: [] };
-  }
-
-  if (type === "CommentCreated") {
-    const { id, content, postId } = data;
-
-    const post = posts[postId];
-    post.comments.push({ id, content });
-  }
-
-  console.log(posts);
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen("4002", (req, res) => {
+app.listen(4002, async () => {
   console.log("Listening on 4002");
+
+  const res = await axios.get("http://localhost:4005/events");
+
+  for (let event of res.data) {
+    console.log("Processing event", event.type);
+
+    handleEvent(event.type, event.data);
+  }
 });
